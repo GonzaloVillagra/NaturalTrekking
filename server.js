9eaa631fec36a1414b5fc137ea03055e301ea35d
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2025-07-09 17:14:46 -0400

    Prueba nueva
    nuevo serversql

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Orígenes permitidos para CORS
 const allowedOrigins = [
   'http://localhost:5000', 
-  'http://localhost:3000', // Permitir localhost:3000 para desarrollo
+  'http://localhost:3000', 
   'https://naturaltrekking.onrender.com'
 ];
 
