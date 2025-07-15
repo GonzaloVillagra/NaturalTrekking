bba72ae16ed6ca2541559085794125a331e0bfcc
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2025-07-14 23:09:36 -0400

    prueba 12

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -6,8 +6,8 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false},
+    family: 'IPv4'
 });
 
 pool.on('connect', () => {
